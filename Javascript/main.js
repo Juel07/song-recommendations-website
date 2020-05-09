48c2415d54654b4ec57044766a5df51b6cca24c4
Juel07 <60902114+Juel07@users.noreply.github.com>
2020-05-10 00:57:10 +0100

    Added code for song-grid

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,26 +1,29 @@
 $(document).ready(function () {
 
-
-    let $btns = $('.project-area .button-group button');
+// song-grid
+    let $btns = $('.grid-area .button-group button');
 
 
     $btns.click(function (e) {
 
-        $('.project-area .button-group button').removeClass('active');
+        $('.grid-area .button-group button').removeClass('active');
         e.target.classList.add('active');
 
         let selector = $(e.target).attr('data-filter');
-        $('.project-area .grid').isotope({
+        $('.grid-area .grid').isotope({
             filter: selector
         });
 
         return false;
     })
 
-    $('.project-area .button-group #btn1').trigger('click');
+    $('.grid-area .button-group #btn1').trigger('click');
 
-    $('.project-area .grid .test-popup-link').magnificPopup({
+    $('.grid-area .grid .test-popup-link').magnificPopup({
         type: 'image',
         gallery: { enabled: true }
     });
+
+// song grid
+
 })
